60856f3ca26fa4d4fb40133db06ffda2c2abae36
Ian Phillips <ianjhphillips@gmail.com>
2020-11-15 11:10:22 -0500

    Created java project, fixed gitignore, and started command line args input

---
+++ ReceiptReader/src/receiptReader/Main.java
@@ -0,0 +1,11 @@
+package receiptReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("ERROR: Please enter receipt folder directory as command argument.");
+            System.exit(1);
+        }
+    }
+}
