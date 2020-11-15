0d2e6361632a4b66ddbab862c109ac827da27273
Ian Phillips <ianjhphillips@gmail.com>
2020-11-15 16:27:12 -0500

    refactored main argument handler block

--- src/ReceiptReader.java
+++ src/ReceiptReader.java
@@ -30,62 +30,20 @@ public class ReceiptReader {
         * If main is called with arguments initialize as well as a file path, initialize the AVL tree.
         * */
         else if (args[0].equals("initialize") && args.length == 2) {
-
-            //Get folder from input
-            filePath = args[1];
-            System.out.println("Initializing Tree with csv folder: " + filePath);
-            File csvDir = new File(filePath);
-
-            //If folder was not found, display an error and terminate the program.
-            if (!csvDir.exists()) {
-                System.out.println("ERROR: Input folder not found. Please verify that input folder is in top level project folder next to /src");
-                System.exit(1);
-            }
-
-            //Create an array of files within the folder and pass the to the AVL tree initializer method
-            File[] dirList = csvDir.listFiles();
-            if (dirList != null) {
-                //TODO Initialize AVL tree from csv file array
-            }
-
+            InitializeAVL(args);
         }
 
         /*
         * If main is called with one argument, take that argument to be the JSON receipts folder path and parse data.
         * */
         else {
-
-            //Gets the avl tree file to be deserialized and checks if null
-            avlTree = DeserializeAVLTree();
-            if (avlTree == null) {
-                System.out.println("ERROR: AVL Tree has not yet been initialized.");
-                System.exit(1);
-            }
-
-            //Get folder from input
-            System.out.println("parsing json");
-            filePath = args[0];
-            File jsonFolder = new File(filePath);
-
-            //If folder was not found, display an error and terminate the program.
-            if (!jsonFolder.exists()) {
-                System.out.println("ERROR: Input folder not found. Please verify that input folder is in top level project folder next to /src");
-                System.exit(1);
-            }
-
-            //Create an array of JSON files within the folder and loop through them, parsing each JSON file.
-            File[] dirList = jsonFolder.listFiles();
-            if (dirList != null) {
-                for (File child : dirList) {
-                    //TODO: parse json using child (.json files)
-                }
-            }
+            ParseReceipts(args);
         }
     }
 
     /**
      * Deserializes the AVLTree object from tree.ser file.
-     * @return AVLTree populated with data from csv files
+     * @return AVLTree populated with data from csv files.
      */
     private static AVLTree DeserializeAVLTree() {
         try {
@@ -101,6 +59,61 @@ public class ReceiptReader {
         } catch (ClassNotFoundException c) {
             c.printStackTrace();
             return null;
+        }
+    }
+
+    /**
+     * Initializes the AVL Tree given a csv folder path as args.
+     * @param args the arguments passed to main() by the user.
+     */
+    private static void InitializeAVL (String[] args) {
+        //Get folder from input
+        filePath = args[1];
+        System.out.println("Initializing Tree with csv folder: " + filePath);
+        File csvDir = new File(filePath);
+
+        //If folder was not found, display an error and terminate the program.
+        if (!csvDir.exists()) {
+            System.out.println("ERROR: Input folder not found. Please verify that input folder is in top level project folder next to /src");
+            System.exit(1);
+        }
+
+        //Create an array of files within the folder and pass the to the AVL tree initializer method
+        File[] dirList = csvDir.listFiles();
+        if (dirList != null) {
+            //TODO Initialize AVL tree from csv file array
+        }
+    }
+
+    /**
+     * Deserializes the AVLTree then parses Data given by input folder path provided by user.
+     * @param args the arguments passed to main() by the user.
+     */
+    private static void ParseReceipts (String[] args) {
+        //Gets the avl tree file to be deserialized and checks if null
+        avlTree = DeserializeAVLTree();
+        if (avlTree == null) {
+            System.out.println("ERROR: AVL Tree has not yet been initialized.");
+            System.exit(1);
+        }
+
+        //Get folder from input
+        System.out.println("parsing json");
+        filePath = args[0];
+        File jsonFolder = new File(filePath);
+
+        //If folder was not found, display an error and terminate the program.
+        if (!jsonFolder.exists()) {
+            System.out.println("ERROR: Input folder not found. Please verify that input folder is in top level project folder next to /src");
+            System.exit(1);
+        }
+
+        //Create an array of JSON files within the folder and loop through them, parsing each JSON file.
+        File[] dirList = jsonFolder.listFiles();
+        if (dirList != null) {
+            for (File child : dirList) {
+                //TODO: parse json using child (.json files)
+            }
         }
     }
 }
