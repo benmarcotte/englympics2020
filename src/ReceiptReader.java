e433cecdc256e0d25c43e08c6fd1944acf9a880b
Ian Phillips <ianjhphillips@gmail.com>
2020-11-15 11:56:12 -0500

    added argument handling

--- src/ReceiptReader.java
+++ src/ReceiptReader.java
@@ -1,8 +1,14 @@
 public class ReceiptReader {
     public static void main(String[] args) {
-        if (args.length < 1) {
+        if (args.length != 1) {
             System.out.println("ERROR: Please enter receipt folder directory as command argument.");
             System.exit(1);
+        }
+        else if (args[0].equals("initialize")) {
+            //Run tree initializing method.
+        }
+        else {
+            //Run main json parsing method.
         }
     }
 }
