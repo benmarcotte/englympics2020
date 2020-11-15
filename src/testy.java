bf09551ac6ace7b1d771671bb2ebbf980bf05aae
Evan Dimopoulos <evan.dime@gmail.com>
2020-11-15 11:46:59 -0500

    adding a class

---
+++ src/testy.java
@@ -0,0 +1,8 @@
+public class testy {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("ERROR: Please enter receipt folder directory as command argument.");
+            System.exit(1);
+        }
+    }
+}
