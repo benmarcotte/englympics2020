75dcd071c33a68fd4646e3f795ac5747f5f9d1cc
Ian Phillips <ianjhphillips@gmail.com>
2020-11-15 11:51:33 -0500

    refactored main class name

--- src/ReceiptReader.java
+++ src/ReceiptReader.java
@@ -1,4 +1,4 @@
-public class testy {
+public class ReceiptReader {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("ERROR: Please enter receipt folder directory as command argument.");
